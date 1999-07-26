4d21319d06cbf75b38d8bb06c74d4c26cffac6a2
Kent Engström <kent@lysator.liu.se>
1999-07-26 13:29:26 +0000

    exclusive_open, exclusive_close: allow direct access to the underlying shelf, for transactions that require more than one shelf operation.

--- lockingshelve.py
+++ lockingshelve.py
@@ -1,5 +1,5 @@
 # Locking version of shelve
-# $Id: lockingshelve.py,v 1.1 1999/07/19 16:31:11 kent Exp $
+# $Id: lockingshelve.py,v 1.2 1999/07/26 13:29:26 kent Exp $
 # (C) 1999 Kent Engström. Released under GPL.
 # NOTE! Just enough for the needs of komimportmail. Use with caution.
 
@@ -8,42 +8,50 @@ import lockfile
 
 class LockingShelve:
     def __init__(self, shelf_name):
-        self.shelf_name = shelf_name
-        self.lock = lockfile.Lock(shelf_name + ".lock")
+        self.__shelf_name = shelf_name
+        self.__lock = lockfile.Lock(shelf_name + ".lock")
 
+    # Internal operations
     def __open_read(self):
-        self.lock.shared_lock()
-        self.shelf = shelve.open(self.shelf_name)
+        self.__lock.shared_lock()
+        self.__shelf = shelve.open(self.__shelf_name)
 
     def __open_write(self):
-        self.lock.exclusive_lock()
-        self.shelf = shelve.open(self.shelf_name)
+        self.__lock.exclusive_lock()
+        self.__shelf = shelve.open(self.__shelf_name)
 
     def __close(self):
-        self.shelf.close()
-        self.shelf = None
-        self.lock.unlock()
+        self.__shelf.close()
+        self.__shelf = None
+        self.__lock.unlock()
 
+    # Locking interface for normal use
     def keys(self):
         self.__open_read()
         try:
-            return self.shelf.keys()
+            return self.__shelf.keys()
         finally:
             self.__close()
             
     def __getitem__(self, index):
         self.__open_read()
         try:
-            return self.shelf[index]
+            return self.__shelf[index]
         finally:
             self.__close()
             
     def __setitem__(self, index, data):
         self.__open_write()
         try:
-            self.shelf[index] = data
+            self.__shelf[index] = data
             import time
         finally:
             self.__close()
-            
-            
+
+    # Use these when you want to use the shelf object directly
+    def exclusive_open(self):
+        self.__open_write()
+        return self.__shelf
+
+    def exclusive_close(self):
+        self.__close()

edc333af2e4be53b98fae0c40f398c7c3108a7cd
Kent Engström <kent@lysator.liu.se>
1999-10-09 14:18:55 +0000

    Lock.__init__: Solaris requires the file to be opened for writing to allow an exclusive lock.

--- lockfile.py
+++ lockfile.py
@@ -1,5 +1,5 @@
 # Primitive lock file handling 
-# $Id: lockfile.py,v 1.1 1999/07/19 16:31:09 kent Exp $
+# $Id: lockfile.py,v 1.2 1999/10/09 14:18:55 kent Exp $
 # (C) 1999 Kent Engström. Released under GPL.
 # NOTE! Just enough for the needs of lockingshelve. Use with caution.
 
@@ -10,7 +10,7 @@ class Lock:
 
     def __init__(self, lock_filename):
         self.filename = lock_filename
-        self.fd = os.open(self.filename, os.O_RDONLY|os.O_CREAT)
+        self.fd = os.open(self.filename, os.O_RDWR|os.O_CREAT)
         
     def shared_lock(self):
         fcntl.flock(self.fd, fcntl.LOCK_SH)

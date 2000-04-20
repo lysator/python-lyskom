38bdc3b9bc40a58c2c02afa324d216e10d9dae60
Kent Engström <kent@lysator.liu.se>
2000-04-20 08:33:45 +0000

    Provide header decoding of ?B?, not just ?Q?. Use built-in quopri module for ?Q? decoing (via cStringIO).

--- rfc1522.py
+++ rfc1522.py
@@ -1,5 +1,5 @@
 # Handling of RFC1522 coding in mail headers
-# $Id: rfc1522.py,v 1.2 1999/07/19 18:37:05 kent Exp $
+# $Id: rfc1522.py,v 1.3 2000/04/20 08:33:45 kent Exp $
 # (C) 1999 Kent Engström. Released under GPL.
 
 # NOTE! Just enough for the needs of komimportmail. Use with caution.
@@ -9,12 +9,15 @@
 
 import re
 import string
+import base64
+import quopri
+import cStringIO
 
 decodable_charsets = ["ISO-8859-1","US-ASCII"]
-re_coded = re.compile("=\?([^?]*)\?[qQ]\?([^?]*)\?=")
+re_coded = re.compile("=\?([^?]*)\?([qQbB])\?([^?]*)\?=")
 
 def decode(str):
-    # Scan the input string, converting QP-coded parts to normal
+    # Scan the input string, converting QP/Base64-coded parts to normal
     # 8-bit strings.
     # Example: "=?ISO-8859-1?Q?Sm=E5_r=E4ksm=F6rg=E5sar?="
     # will become "Små räksmörgåsar"
@@ -25,32 +28,23 @@ def decode(str):
         if not m:
             break
 
-        (charset, data) = m.group(1,2)
+        (charset, coding, data) = m.group(1,2,3)
         if string.upper(charset) not in decodable_charsets:
             start = m.end(0)
             continue # Look for more coded parts to try to convert
 
-        decoded = decode_qp(data)
+        if coding in "qQ":
+            decoded = decode_qp(data)
+        else:
+            decoded = base64.decodestring(data)
         str = str[:m.start(0)] + decoded + str[m.end(0):]
         start = m.start(0) + len(decoded)
 
     return str
 
 def decode_qp(str):
-    # Decode the input string from QP to normal 8-bit text, handling both
-    # "=HH" (hex coded) and "_" (space).
-
-    res = []
-    i = 0
-    while i < len(str):
-        if str[i] == "_":
-            res.append(" ")
-        elif str[i] == "=" and i < len(str) - 2 and \
-             str[i+1] in string.hexdigits and str[i+2] in string.hexdigits:
-            res.append(chr(eval("0x" + str[i+1:i+3])))
-            i = i + 2
-        else:
-            res.append(str[i])
-        i = i + 1
-    return string.join(res,"")
-                
+    # The quopri modules does not provide string versions of the functions
+    infile = cStringIO.StringIO(str)
+    outfile = cStringIO.StringIO()
+    quopri.decode(infile, outfile)
+    return outfile.getvalue()

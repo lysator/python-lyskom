ae4aac93730fbbbde50f6dd5c10752db7e7be80f
Erik Forsberg <forsberg@lysator.liu.se>
2002-03-03 19:43:50 +0000

    Added some more comments that might be helpful for the new komimportmail user.

--- komimportmail_config_template.py
+++ komimportmail_config_template.py
@@ -2,17 +2,25 @@ import kom
 
 # -- CONFIGURATION BEGINS HERE ---
 
-# How to connect to the server
-KOMSERVER = "localhost"     # Name or IP address
+# How to connect to the server. 
+KOMSERVER = "FIXME"     # Name or IP address
 KOMPORT = 4894		    # Port number
+
+# The LysKOM person that's author of the imported mails. Should
+# preferably be a person with only mailimport as responsibility.
 KOMPERSON = FIXME           # LysKOM person number
 KOMPASSSWORD = "FIXME"      # Password of KOMPERSON
 
-# File to log some information into
+# File to log some information into. Make sure the user executing
+# komimportmail has write-permissions in the directory where this file
+# resides.
 LOG_FILE = "/FIXME/maillog"
 
 # File for message-id -> text-no database
-# Note: the actual filename depends on what database shelve.py chooses.
+# Note: the actual filename depends on what database shelve.py
+# chooses.
+# As for the LOG_FILE, the user running komimportmail must have
+# write-permissions here too.
 ID_DB_FILE = "/FIXME/message-id"
 
 # Text to prepend to subject header of appendices
@@ -25,7 +33,8 @@ APPENDIX_COMMENT_TYPE = kom.MIC_COMMENT #or kom.MIC_FOOTNOTE
 # Skip AI_MX_DATE (for use with buggy aux-items.conf:s)
 SKIP_AI_MX_DATE = 0
 
-# Remove reduntant text/html after text/plain in multipart/alternative:
+# Remove reduntant text/html after text/plain in mails with
+# MIME-parts with content-type multipart/alternative.
 REMOVE_REDUNDANT_HTML = 1
 
 # -- CONFIGURATION ENDS HERE ---
